9d7b542d12f83f82dc0366a8fd050123cd6b5e72
rukia <ruqayashar1@gmail.com>
2023-05-19 13:06:47 +0300

    setter and getter

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
-
-end
+    # setter method for name
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+  
+    # setter method for breed
+    def breed=(breed)
+      @breed = breed
+    end
+  
+    # getter method
+    def breed
+      @breed
+    end
+  end
+ 
+  
+  
+  
+  
+  
+  
+  
